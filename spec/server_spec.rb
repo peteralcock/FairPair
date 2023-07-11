983a996e81097dd013a5854fb583ce26b50b7aa5
Peter Alcock <peteralcock@users.noreply.github.com>
2023-07-11 04:58:52 -0400

    Create server_spec.rb

---
+++ spec/server_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'PostgreSQL service' do
+  it 'is running on port 5432' do
+    expect(port(5432)).to be_listening
+  end
+end
+
+describe 'Redis service' do
+  it 'is running on port 6379' do
+    expect(port(6379)).to be_listening
+  end
+end
+
+describe 'Access to www.themealdb.com' do
+  it 'is available' do
+    host = 'www.themealdb.com'
+    expect(host(host)).to be_reachable
+  end
+end
