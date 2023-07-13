03b167423de31ebdaceb62cd0c575ea089595712
Peter Alcock <admin@peteralcock.com>
2023-07-13 04:21:19 -0400

    Fix developer relations...

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,14 +1,17 @@
 class Developer < ApplicationRecord
   validates_presence_of :name
   # Sprint. Singular. ONE thing at a time...
-  belongs_to :sprint, :class_name => "Sprint", :optional => true
+  has_one :sprint, :class_name => "Sprint"
   # has_many :pairings, :through => :sprint
   has_many :pto_requests
   #has_many :pairings, class_name: 'Pairing', foreign_key: 'developer1_id' || 'developer2_id'
-  # has_many :pairings, -> { where(developer1_id: 'id') ||  where(developer2_id: 'id' )}, class_name: 'Pairing'
   has_many :pto_requests
   has_many :pairings_as_dev1, class_name: 'Pairing', foreign_key: 'developer1_id'
   has_many :pairings_as_dev2, class_name: 'Pairing', foreign_key: 'developer2_id'
+  #  has_many :pairings, -> { where(developer1_id: 'id') ||  where(developer2_id: 'id' )}, class_name: 'Pairing'
+  has_many :pairings, :through => :sprint
+  has_many :projects, :through => :pairings
+  has_many :sprints, :through => :projects
 
   def pairings
     Pairing.where("developer1_id = ? OR developer2_id = ?", id, id)
